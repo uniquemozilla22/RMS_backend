237c9990e9c5c2e9232d84a8a9110310ce9666c3
uniquemozilla22 <uniqgaming26@gmail.com>
2022-09-23 02:35:15 +0545

    Login Comleted with handled response

--- src/routes/Global/Login.global.ts
+++ src/routes/Global/Login.global.ts
@@ -1,9 +1,16 @@
 import { Request, Response } from "express";
+import { IUser } from "../../database/Schema/User/User.schema";
+import { LoginSuccessfull } from "../../utils/HandleResponse/HandleResponse.utils";
 import { createToken } from "../../utils/middleware/jwt.middleware";
 
 const LoginGlobalRoute = (req: Request, res: Response) => {
-  const { user } = req.body;
-  const token = createToken(user);
+  const { username, name, address, phone, userType }: IUser = req.body.user;
+  const user = { username, name, address, phone, userType };
+  const token: string = createToken(user);
+
+  res
+    .status(200)
+    .send(LoginSuccessfull("Login Successfull", { ...user, token }));
 };
 
 export default LoginGlobalRoute;
--- src/utils/HandleResponse/HandleResponse.utils.ts
+++ src/utils/HandleResponse/HandleResponse.utils.ts
@@ -1,3 +1,25 @@
+export interface ISuccessMessage {
+  success: boolean;
+  status: number;
+  message: string;
+  data?: any;
+}
 
-
-export const SuccesMessageWithData = (message:string,data:any)=> ({success:true, status : 200 , message, data})
+export const SuccesMessageWithData = (
+  message: string,
+  data: any
+): ISuccessMessage => ({
+  success: true,
+  status: 200,
+  message,
+  data,
+});
+export const LoginSuccessfull = (
+  message: string,
+  data: any
+): ISuccessMessage => ({
+  success: true,
+  status: 200,
+  message,
+  data,
+});

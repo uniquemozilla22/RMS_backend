7c28732ccddab2ba880b2eaa6220e57db1d95923
uniquemozilla22 <uniqgaming26@gmail.com>
2022-10-02 15:38:52 +0545

    Added Notification Schema

---
+++ src/database/Schema/Notifications/Notification.schema.ts
@@ -0,0 +1,25 @@
+import { InferSchemaType, model, Schema } from "mongoose";
+
+const NotificationList = new Schema({
+  notification_type: {
+    type: "string",
+    required: true,
+  },
+  message: {
+    type: "string",
+    required: true,
+  },
+  time: {
+    type: "string",
+    required: true,
+  },
+});
+
+export type INotificationList = InferSchemaType<typeof NotificationList>;
+
+const Notification = new Schema({
+  lists: [NotificationList],
+});
+const NotificationSchema = model("notifications", Notification);
+
+export default NotificationSchema;
